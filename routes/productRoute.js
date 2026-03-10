8dc9ff12fe87579f18eefe29694a3116d58b898c
Charitha-Reddy1 <carg2800@gmail.com>
2026-03-10 13:59:11 +0530

    feat: add product management functionality with create, edit, and delete operations

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,13 @@
-import {getProducts} from "../controllers/productController.js";
+import {getProducts,addProduct,addProductForm,deleteProduct,editProductForm,saveProduct} from "../controllers/productController.js";
 import express from "express"
 
 const productRouter=express.Router()
 
 productRouter.get("/",getProducts)
-
+productRouter.get("/add",addProductForm)
+productRouter.post("/add",addProduct)
+productRouter.get("/:id/delete",deleteProduct)
+productRouter.get("/:id/edit",editProductForm)
+productRouter.post("/:id/save",saveProduct)
 
 export {productRouter}
