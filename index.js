9661f328385ea100ed3c9528f259784ac8467995
Charitha-Reddy1 <carg2800@gmail.com>
2026-03-10 11:18:33 +0530

    feat: implement product listing functionality with EJS rendering

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv"
 dotenv.config()
 import mongoose from "mongoose"
 import dbConnect from "./config/db.js"
-// import {productRouter} from "./routes/productRoute.js";
+import {productRouter} from "./routes/productRoute.js";
 import { storeRouter } from "./routes/storeRoute.js";
 
 
@@ -27,7 +27,7 @@ app.use(
 
 app.use("/", storeRouter);
 // app.use("/auth", authRouter);
-// app.use("/products", productRouter);
+app.use("/products", productRouter);
 // app.use("/users", userRouter);
 
 
---
+++ routes/productRoute.js
@@ -0,0 +1,9 @@
+import {getProducts} from "../controllers/productController.js";
+import express from "express"
+
+const productRouter=express.Router()
+
+productRouter.get("/",getProducts)
+
+
+export {productRouter}
